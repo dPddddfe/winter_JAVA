291323e89d2a351ee5b7e80ce9bd5c320a60b501
yewon_winterJava <ysewon2004@naver.com>
2025-01-02 21:52:50 +0900

    main

---
+++ A/A01.java
@@ -0,0 +1,21 @@
+package A;
+import java.util.Scanner;
+
+class A01 {
+    public static void main(String[] args){
+        A01 stepA = new A01();
+        stepA.a01();
+    }
+
+    void a01(){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a birth year > ");
+        int birth_year = sc.nextInt();
+        int age = 2025 - birth_year + 1;
+
+        System.out.println("You are "+age+" year's old.");
+
+    }
+    
+}
